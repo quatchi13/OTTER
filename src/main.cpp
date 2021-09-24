020b0fa577432a8b25061076347dcb7c60faa345
Isaac <quatchimanisaac@gmail.com>
2021-09-24 13:18:43 -0400

    Here is some epic things from CG tut 2

---
+++ src/main.cpp
@@ -0,0 +1,102 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <fstream> //03
+#include <string> //03
+
+GLFWwindow* window;
+
+bool initGLFW() {
+	if (glfwInit() == GLFW_FALSE) {
+		std::cout << "Failed to Initialize GLFW" << std::endl;
+		return false;
+	}
+
+	//Create a new GLFW window
+	window = glfwCreateWindow(800, 800, "INFR1350U", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+
+	return true;
+}
+
+bool initGLAD() {
+	if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress) == 0) {
+		std::cout << "Failed to initialize Glad" << std::endl;
+		return false;
+	}
+}
+
+
+GLuint shader_program;
+
+bool loadShaders() {
+	// Read Shaders from file
+	std::string vert_shader_str;
+	std::ifstream vs_stream("vertex_shader.glsl", std::ios::in);
+	if (vs_stream.is_open()) {
+		std::string Line = "";
+		while (getline(vs_stream, Line))
+			vert_shader_str += "\n" + Line;
+		vs_stream.close();
+	}
+	else {
+		printf("Could not open vertex shader!!\n");
+		return false;
+	}
+	const char* vs_str = vert_shader_str.c_str();
+
+	std::string frag_shader_str;
+	std::ifstream fs_stream("frag_shader.glsl", std::ios::in);
+	if (fs_stream.is_open()) {
+		std::string Line = "";
+		while (getline(fs_stream, Line))
+			frag_shader_str += "\n" + Line;
+		fs_stream.close();
+	}
+	else {
+		printf("Could not open fragment shader!!\n");
+		return false;
+	}
+	const char* fs_str = frag_shader_str.c_str();
+
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vs_str, NULL);
+	glCompileShader(vs);
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fs_str, NULL);
+	glCompileShader(fs);
+
+	shader_program = glCreateProgram();
+	glAttachShader(shader_program, fs);
+	glAttachShader(shader_program, vs);
+	glLinkProgram(shader_program);
+
+	return true;
+}
+
+
+
+int main() {
+	//Initialize GLFW
+	if (!initGLFW())
+		return 1;
+
+	//Initialize GLAD
+	if (!initGLAD())
+		return 1;
+
+	///// Game loop /////
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		
+
+		
+		glfwSwapBuffers(window);
+	}
+	return 0;
+
+}
